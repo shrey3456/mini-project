f5fa02f808250ebb4c2e3abaca5e9bf24210739d
shrey3456 <22it111@charusat.edu.in>
2024-04-01 11:27:56 +0530

    initial commit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,18 +5,18 @@ const Footer = () => {
     <footer>
       <div className="container">
         <div className="banner">
-          <div className="left">ZEESHU</div>
+          <div className="left">Shrey</div>
           <div className="right">
-            <p>Z6 Gulshan-e-Maymar, Karachi</p>
+            <p>Happiness Bilimora</p>
             <p>Open: 05:00 PM - 12:00 AM</p>
           </div>
         </div>
         <div className="banner">
           <div className="left">
-            <p>Developed By CODEWITHZEESHU</p>
+            <p>Developed By Shrey</p>
           </div>
           <div className="right">
-            <p>All Rights Reserved By CodeWithZeeshu.</p>
+            <p>All Rights Reserved By Shrey.</p>
           </div>
         </div>
       </div>
